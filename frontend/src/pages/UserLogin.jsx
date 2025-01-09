2e08a3c5b997e5f145ed33e54f9a9e900c833912
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2025-01-10 00:34:36 +0530

    Add User Login Page

---
+++ frontend/src/pages/UserLogin.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { LOGIN_LOGO } from "../utils/constants";
+
+const UserLogin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userData, setUserData] = useState({});
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setUserData({ email, password });
+    console.log(userData);
+    setEmail("");
+    setPassword("");
+  };
+  return (
+    <div className="p-7 h-screen flex flex-col justify-between">
+      <div>
+        <img className="w-16 mb-10" src={LOGIN_LOGO} alt="" />
+        <form
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}
+        >
+          <h3 className="text-lg font-medium mb-2">What&apos;s your email</h3>
+          <input
+            required
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            className="bg-gray-200 mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base"
+            type="email"
+            placeholder="email@example.com"
+          />
+          <h3 className="text-lg font-medium mb-2">Enter Password</h3>
+          <input
+            className="bg-gray-200 mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            required
+            type="password"
+            placeholder="password"
+          />
+          <button className="bg-black text-white font-semibold mb-3 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base">
+            Login
+          </button>
+        </form>
+        <p className="text-center">
+          Don&apos;t have an account?{" "}
+          <Link to="/signup" className="text-blue-600">
+            Create new Account
+          </Link>
+        </p>
+      </div>
+      <div>
+        <Link
+          to="/captain-login"
+          className="bg-green-500 flex items-center justify-center text-white font-semibold mb-5 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base hover:bg-green-600"
+        >
+          Sign in as Captain
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default UserLogin;
